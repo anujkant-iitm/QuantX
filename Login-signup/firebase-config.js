a8624fdb0d9268dfde9bfa327e0fc622032791cb
Anujkant-iitm <anujkant.iitm@gmail.com>
2025-09-14 12:02:21 +0530

    login

--- Login-signup/firebase-config.js
+++ Login-signup/firebase-config.js
@@ -1,7 +1,12 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAWa3itQgOYerg-xh1FHvYx8_xPROFmIpM",
     authDomain: "quantx-studio.firebaseapp.com",
@@ -12,7 +17,6 @@ const firebaseConfig = {
     measurementId: "G-BL772GV8MN"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth };
+const analytics = getAnalytics(app);
